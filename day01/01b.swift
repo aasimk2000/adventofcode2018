65b1b54f2e74080ebb1262669f562986e11afcfe
aasimk <aasimk@umich.edu>
2018-12-01 17:54:14 -0500

    Added comments to 01b

--- day01/01b.swift
+++ day01/01b.swift
@@ -4,22 +4,28 @@ import Foundation
 let path = "input"
 var input = [String]()
 
+// Store input data in string. Then split string into string array based on new line
 if let rawInput = try? String(contentsOfFile: path) {
     input = rawInput.components(separatedBy: "\n")
 }
 
+// Convert to integer array
 let numericalInput = input.compactMap {Int(String($0))}
 
-var f = 0
+var total = 0
+
+// Create integer set
 var frequencies = Set<Int>([0])
+// Infinite loop
 repeat {
     for num in numericalInput {
-        f = f + num
-        if frequencies.contains(f) {
-            print(f)
+        total += num
+        // If number in frequncies print and quit
+        if frequencies.contains(total) {
+            print(total)
             exit(1)
         } else {
-            frequencies.insert(f)
+            frequencies.insert(total)
         }
     }
 } while true
