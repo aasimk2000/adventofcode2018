15f2695962fd07033d4a84cd6738fb7ff8098e04
aasimk <aasimk@umich.edu>
2018-12-04 22:31:07 -0500

    Added comments for day03

--- day03/03.swift
+++ day03/03.swift
@@ -1,10 +1,12 @@
 import Foundation
 
+// Declare a new struct
 struct Point {
     var xCoord = 0
     var yCoord = 0
 }
 
+// Make struct hashable to allow it to be inserted into Sets
 extension Point: Hashable {
     public var hashValue: Int {
         return xCoord.hashValue ^ yCoord.hashValue
@@ -15,6 +17,7 @@ extension Point: Hashable {
     }
 }
 
+// Rectangle struct used to keep code organized
 struct Rectangle {
     var startX = 0
     var startY = 0
@@ -22,6 +25,7 @@ struct Rectangle {
     var height = 0
 }
 
+// Takes in regex and string outputs matching string array
 func matches(for regex: String, in text: String) -> [String] {
     do {
         let regex = try NSRegularExpression(pattern: regex)
@@ -36,6 +40,14 @@ func matches(for regex: String, in text: String) -> [String] {
     }
 }
 
+// Uses the regex function above to parse Input and output a rectangle
+func parseInput(_ input: String) -> Rectangle {
+    let matched = matches(for: "[0-9]+", in: input)
+    return Rectangle(startX: Int(matched[1])!, startY: Int(matched[2])!,
+                     width: Int(matched[3])!, height: Int(matched[4])!)
+}
+
+// Outputs a set of points which contains the claim of cloth
 func getPoints(_ rect: Rectangle) -> Set<Point> {
     let size = rect.width * rect.height
     var coordinates = Set<Point>(minimumCapacity: size)
@@ -47,12 +59,6 @@ func getPoints(_ rect: Rectangle) -> Set<Point> {
     return coordinates
 }
 
-func parseInput(_ input: String) -> Rectangle {
-    let matched = matches(for: "[0-9]+", in: input)
-    return Rectangle(startX: Int(matched[1])!, startY: Int(matched[2])!,
-                     width: Int(matched[3])!, height: Int(matched[4])!)
-}
-
 let path = "input"
 var input = [String]()
 
@@ -61,17 +67,25 @@ if let rawInput = try? String(contentsOfFile: path) {
     input = rawInput.components(separatedBy: "\n")
 }
 
+// Sets to store coodinates seen, and those that have duplicates
 var seen = Set<Point>()
 var intersecting = Set<Point>()
+
+// Iterates through all non empty inputs
+// Gets points into a set, Appends intersection between points and seen to intersecting
+// Adds rest to seen
 for index in input where index != "" {
     let temp = getPoints(parseInput(index))
     intersecting = intersecting.union(temp.intersection(seen))
     seen = seen.union(temp)
 }
-print(intersecting.count)
 
+// Prints number of intersecting points
+print("Part A:\(intersecting.count)")
+
+// Iterates through all non empty inputs and finds line that is disjoint with intersecting prints ID
 for index in input where index != "" {
     if intersecting.isDisjoint(with: getPoints(parseInput(index))) {
-        print(index)
+        print("Part B:\(matches(for: "[0-9]+", in: index)[0])")
     }
 }
