34ba2c3916a430fd09f41f757c2cf2e89bafe910
aasimk <aasimk@umich.edu>
2018-12-07 22:36:29 -0500

    Finished up day 4

--- day04/04.swift
+++ day04/04.swift
@@ -1,48 +1,73 @@
 import Foundation
 
-struct GuardInfo {
-    let ID:Int
-    let start:Date
-    let end:Date
-}
-
 let path = "input"
 var input = [String]()
-
-var Guards = [GuardInfo]()
-let times = [Int:Int]()
 
 // Convert file to string and then store every new line in new string in input array
 if let rawInput = try? String(contentsOfFile: path) {
     input = rawInput.components(separatedBy: "\n")
 }
 
+// Stores guard number and array of minutes during which they are sleeping
+var guardSleep = [Int: [Int]]()
+// Stores guard number and total time asleep
+var times = [Int: Int]()
+
+// Some variables that are used later
+var start = Int()
+var end = Int()
+var guardNumber = Int()
+
+// Sorting the input
 input.sort()
 for line in input where line != "" {
     var guardData = line.components(separatedBy: "]")
+    // Seperate time and action
     var time = guardData[0]
-    time.remove(at: time.startIndex)
-    let date = dateFormatter.date(from: time)
     let action = guardData[1]
-    var guardNumber = Int()
-    var start = Date()
-    var end = Date()
-    
+    time.remove(at: time.startIndex)
+    guardData = time.components(separatedBy: ":")
+    time = guardData[1]
+    let minute = Int(time)!
+
     if action.hasPrefix(" Guard") {
         var actionData = action.components(separatedBy: " ")
-        var guardString = actionData[2]
-        guardString.remove(at: guardString.startIndex)
-        guardNumber = Int(guardString)!
+        var guardSleeptring = actionData[2]
+        guardSleeptring.remove(at: guardSleeptring.startIndex)
+        guardNumber = Int(guardSleeptring)!
+        // Initialize array for guard number
+        if !guardSleep.keys.contains(guardNumber) {
+            guardSleep[guardNumber] = [Int]()
+        }
     } else if action.hasPrefix(" falls") {
-        start = date!
+        start = minute
     } else if action.hasPrefix(" wakes up") {
-        end = date!
-        let thisGuard = GuardInfo(ID: guardNumber, start: start, end: end)
-        Guards.append(thisGuard)
-        let test = Calendar.current.dateComponents([.minute], from: start, to: end)
-        //print(test.minute)
+        let end = minute
+        let diff = end - start
+        times[guardNumber] = (times[guardNumber] ?? 0) + diff
+        let sleepTimes: [Int] = Array(start..<end)
+        guardSleep[guardNumber]?.append(contentsOf: sleepTimes)
     }
-    
 }
 
-// [1518-05-04 23:56] Guard #523 begins shift
+// Find guard with greates time sleeping
+let greatestTime = times.max(by: {$0.1 < $1.1})
+var counts = [Int: Int]()
+guardSleep[(greatestTime?.key)!]?.forEach { counts[$0] = (counts[$0] ?? 0) + 1 }
+if let (value, _) = counts.max(by: {$0.1 < $1.1}) {
+    print("Part A: \(value * (greatestTime?.key ?? -1))")
+}
+
+// Store [Guard Number, minute sleeping most]: Count
+var maxSleepTimePerGuard = [[Int]: Int]()
+
+for (guardNumber, sleepTimes) in guardSleep {
+    var counts = [Int: Int]()
+    sleepTimes.forEach {counts[$0] = (counts[$0] ?? 0) + 1}
+    if let (value, count) = counts.max(by: {$0.1 < $1.1}) {
+        maxSleepTimePerGuard[[guardNumber, value]] = count
+    }
+}
+
+let greatestMinute = maxSleepTimePerGuard.max(by: {$0.1 < $1.1})
+print("Part B: \((greatestMinute?.key[1])! * (greatestMinute?.key[0])!)")
